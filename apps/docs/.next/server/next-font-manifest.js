ef7c263f22958f45fabc2525d70b36cc1a11bd4a
Paula Meng <paulameng@MacBook-Pro-8.local>
2025-11-25 16:43:51 +0800

    chore: 添加 .next 目录到 .gitignore

--- apps/docs/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/paulameng/Documents/SmartX 2021/Code/ct-style/apps/docs/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
